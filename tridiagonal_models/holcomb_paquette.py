c956089be0849369bccc5450212fa4361ccaefa2
Sébastien Ohleyer <sebastien.ohleyer@gmail.com>
2017-09-08 13:29:52 +0200

    change tridiag

--- tridiagonal_models/holcomb_paquette.py
+++ tridiagonal_models/holcomb_paquette.py
@@ -8,12 +8,13 @@ from plot_tools import adjust_spines, sc_law, plot_dpp, plot_traj_with_histo, pl
 SEED = 32
 np.random.seed(SEED)
 
-def beta_Hermite_Gaussian_ensemble(dim, beta):
-    diag = np.sqrt(2)*np.diag(np.random.randn(dim))
+def tridiagonal_model_gaussian(dim, beta):
+    diag = (1/np.sqrt(dim))*np.diag(np.random.randn(dim))
     u_diag_vect = np.zeros(dim-1)
     for i in range(0,dim-1):
-        u_diag_vect[i] = np.sqrt(np.random.chisquare((dim-(i+1))*beta/4))
-    u_diag = np.diag(u_diag_vect,1)
+        #u_diag_vect[i] = np.sqrt(np.random.chisquare((dim-(i+1))*beta))
+        u_diag_vect[i] = np.linalg.norm( (1/np.sqrt(2*dim))*np.random.randn((dim-(i+1))*beta) )
+    u_diag = np.diag(u_diag_vect, 1)
     H_b = diag + u_diag + u_diag.T
     return H_b
 
@@ -25,14 +26,13 @@ class tridiag_dyson:
         self.n_samples = n_samples
         self.tf = tf
         self.dt = tf/n_samples
-        init_mat = beta_Hermite_Gaussian_ensemble(self.dim, beta=2)
-        self.tridiag_matrices=[init_mat]
+        init_mat = tridiagonal_model_gaussian(self.dim, beta=2)
+        self.tridiag_matrices = [init_mat]
         self.eigen_values = [sorted(np.linalg.eigvalsh(init_mat), reverse=True)]
-        self.brownians = [ np.zeros(n_traj) ]
+        self.brownians = [np.zeros(n_traj)]
         self.p = None
         self.G = None
         self.O = None
-        self.E = None
         self.generate()
 
     def compute_polynomials(self, U):
@@ -59,13 +59,13 @@ class tridiag_dyson:
         if l <= k:
             pass
         else:
-            for eigenvalue in eigenvalues:
+            for i, eigenvalue in enumerate(eigenvalues):
                 if eigenvalue != lbda :
                     frac = (P.polyval(lbda, self.p[l]) - P.polyval(eigenvalue, self.p[l])) / (lbda-eigenvalue)
-                    p_kl += self.spectral_weight(eigenvalue)**2 * P.polyval(eigenvalue, self.p[k]) * frac
+                    p_kl += self.frozen_spectral_w[i]**2 * P.polyval(eigenvalue, self.p[k]) * frac
                 else:
-                    deriv = P.polyval(lbda, P.polyder(self.p[l]))
-                    p_kl += self.spectral_weight(eigenvalue)**2 * P.polyval(eigenvalue, self.p[k]) * deriv
+                    deriv = P.polyval(eigenvalue, P.polyder(self.p[l]))
+                    p_kl += self.frozen_spectral_w[i]**2 * P.polyval(eigenvalue, self.p[k]) * deriv
         return p_kl
 
     def spectral_weight(self, lbda):
@@ -93,7 +93,7 @@ class tridiag_dyson:
             G[l+1, l] = -U[l, l+1] * (P.polyval(lbda, self.p[l-1]) * P.polyval(lbda, P.polyder(self.p[l-1]))
                                     - P.polyval(lbda, self.p[l]) * P.polyval(lbda, P.polyder(self.p[l])))
 
-        self.G = -(G+np.diag(np.diag(G, -1), +1))
+        self.G = -(G + np.diag(np.diag(G, -1), +1))
         return self.G
 
     def compute_G_kl(self, k, l, U, eigenvalues):
@@ -107,55 +107,45 @@ class tridiag_dyson:
 
         else:
             sum = 0
-            for eigenvalue in eigenvalues:
+            for i, eigenvalue in enumerate(eigenvalues):
                 acc = - U[0,1]*P.polyval(eigenvalue, self.p[0])*self.compute_p_kl(k=1, l=l, lbda=eigenvalue, eigenvalues=eigenvalues)\
                       - U[0,1]*P.polyval(eigenvalue, self.p[1])*self.compute_p_kl(k=0, l=l, lbda=eigenvalue, eigenvalues=eigenvalues)
-                sum += self.spectral_weight(eigenvalue)**2 * P.polyval(eigenvalue, self.p[k]) * acc
+                sum += self.frozen_spectral_w[i]**2 * P.polyval(eigenvalue, self.p[k]) * acc
             G_kl[0,0] = 0.5 * sum
 
 
             for r in range(1, self.dim-1):
                 sum = 0
-                for eigenvalue in eigenvalues:
+                for i, eigenvalue in enumerate(eigenvalues):
                     acc = U[r, r-1] * P.polyval(eigenvalue, self.p[r-1]) * self.compute_p_kl(k=r, l=l, lbda=eigenvalue, eigenvalues=eigenvalues)\
                         - U[r, r+1] * P.polyval(eigenvalue, self.p[r]) * self.compute_p_kl(k=r+1, l=l, lbda=eigenvalue, eigenvalues=eigenvalues)\
                         + U[r, r-1] * P.polyval(eigenvalue, self.p[r]) * self.compute_p_kl(k=r-1, l=l, lbda=eigenvalue, eigenvalues=eigenvalues)\
                         - U[r, r+1] * P.polyval(eigenvalue, self.p[r+1]) * self.compute_p_kl(k=r, l=l, lbda=eigenvalue, eigenvalues=eigenvalues)
 
-                    sum += self.spectral_weight(eigenvalue) ** 2 * P.polyval(eigenvalue, self.p[k]) * acc
+                    sum += self.frozen_spectral_w[i]**2 * P.polyval(eigenvalue, self.p[k]) * acc
                 G_kl[r, r] = 0.5 * sum
 
             for u in range(0, self.dim-2):
                 sum=0
-                for eigenvalue in eigenvalues:
+                for i, eigenvalue in enumerate(eigenvalues):
                     acc = U[r, r+1] * ( P.polyval(eigenvalue, self.p[r]) * self.compute_p_kl(k=r, l=l, lbda=eigenvalue, eigenvalues=eigenvalues)
                                       - P.polyval(eigenvalue, self.p[r+1]) * self.compute_p_kl(k=r+1, l=l, lbda=eigenvalue, eigenvalues=eigenvalues))
 
-                    sum += self.spectral_weight(eigenvalue) ** 2 * P.polyval(eigenvalue, self.p[k]) * acc
+                    sum += self.frozen_spectral_w[i]**2 * P.polyval(eigenvalue, self.p[k]) * acc
                 G_kl[u, u-1] = 0.5 * sum
 
         G_kl_tridiag = (G_kl+np.diag(np.diag(G_kl, -1), +1))
         return G_kl_tridiag
 
-    def compute_E(self, lbda, i):
-        delta_ii = np.zeros((self.dim, self.dim))
-        delta_ii[i, i] = 1
-        O = np.zeros((self.dim, self.dim))
-        for k in range(self.dim):
-            q_k = self.spectral_weight(lbda[k])
-            for l in range(self.dim):
-                O[l, k] = q_k * P.polyval(lbda[k], self.p[l])
-        q_i = self.spectral_weight(lbda[i])
-        E = np.dot(np.dot(O.T, (1 / q_i ** 2) * delta_ii), O)
-        self.O = O
-        self.E = E
-        return E
 
     def compute_brownians(self):
-        draw_brownians = self.brownians[-1] + np.random.randn(self.n_traj) * self.dt ** 0.5
+        draw_brownians = self.brownians[-1] + np.random.randn(self.n_traj) * (self.dt/self.n_traj)**0.5
         self.brownians.append(draw_brownians)
 
     def generate(self):
+        self.compute_polynomials(self.tridiag_matrices[0])
+        self.frozen_spectral_w = [self.spectral_weight(self.eigen_values[0][i]) for i in range(self.n_traj)]
+
         for sample in range(1, self.n_samples+1):
             self.compute_polynomials(self.tridiag_matrices[sample-1])
             self.compute_brownians()
@@ -168,7 +158,7 @@ class tridiag_dyson:
                 eigen_values_list = [lbda for lbda in list(self.eigen_values[sample - 1]) if lbda != lbda_i]
                 sum_term = sum([(1 / (lbda_i - lbda_k)) for lbda_k in eigen_values_list])
 
-                d_A -= (self.brownians[sample][i] + self.dt*(-2*lbda_i + sum_term))*(self.spectral_weight(lbda_i)**2)*G_i
+                d_A -= (self.brownians[sample][i] + self.dt*(-lbda_i/2 + sum_term))*(self.frozen_spectral_w[i]**2)*G_i
             #print(d_A)
 
             d_P = 0
@@ -177,7 +167,7 @@ class tridiag_dyson:
                 for k in range(0, self.dim):
                     for l in range(0, self.dim):
                         add = P.polyval(lbda_i, self.p[k]) * P.polyval(lbda_i, P.polyder(self.p[l])) + P.polyval(lbda_i, P.polyder(self.p[k])) * P.polyval(lbda_i, self.p[l])
-                        d_P -= 0.5 * self.spectral_weight(lbda_i)**2 * add * self.compute_G_kl(k, l, self.tridiag_matrices[sample-1], self.eigen_values[sample-1]) * self.dt
+                        d_P -= 0.5 * self.frozen_spectral_w[i]**2 * add * self.compute_G_kl(k, l, self.tridiag_matrices[sample-1], self.eigen_values[sample-1]) * self.dt
 
             draw_A = self.tridiag_matrices[sample-1] + d_A + d_P
             #print(d_P)
